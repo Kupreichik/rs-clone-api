ca348f30af2e5c53d5d2191c743b04cbd841d926
Julia Kupreichik <julilike@yandex.ru>
2023-02-02 17:04:52 +0300

    feat: connect MongoDB

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:yyyyyy@cluster0.qbtixrj.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('DB connect successfully'))
+  .catch((err) => console.log('DB error', err));
+
+app.listen(3033, (err) => {
+  if (err) return console.log(err);
+  console.log('Server run')
+});
