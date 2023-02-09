3c23ca2e18c275826db785f1076122ef462137ec
Julia Kupreichik <julilike@yandex.ru>
2023-02-09 18:12:09 +0300

    feat: add getting pens

--- controllers/pensController.js
+++ controllers/pensController.js
@@ -1,5 +1,9 @@
 import PenModel from '../models/Pen.js'
 
+const serverError = {
+      message: 'Some server error',
+    };
+
 export const create = async (req, res) => {
   try {
     const doc = new PenModel({
@@ -14,10 +18,35 @@ export const create = async (req, res) => {
 
     const { __v, ...penData } = pen._doc;
     res.json(penData);
+
   } catch (err) {
     console.log(err);
-    res.status(500).json({
-      message: 'Some server error',
-    });
+    res.status(500).json(serverError);
+  }
+};
+
+export const getAll = async (req, res) => {
+    try {
+      const limit = req.query._limit || 10;
+      const data = await PenModel.find()
+        .limit(limit)
+        .populate({
+          path: 'user',
+          select: {
+            name: 1,
+            username: 1,
+            avatar: 1,
+            _id: 0
+          },
+        })
+        .exec();
+
+      const totalCount = await PenModel.find().count();
+
+      res.setHeader('X-Total-Count', totalCount).json(data);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(serverError);
   }
 };
--- routes/pens.js
+++ routes/pens.js
@@ -12,6 +12,48 @@ const router = express.Router();
 *     PenData:
 *       type: object
 *       properties:
+*         _id:
+*           type: string
+*           description: Uniq id of pen
+*         title:
+*           type: string
+*           description: Title of pen
+*         html:
+*           type: string
+*           description: HTML code
+*         css:
+*           type: string
+*           description: CSS code
+*         js:
+*           type: string
+*           description: JavaScript code
+*         likesCount:
+*           type: number
+*           description: Number of pen likes
+*         viewsCount:
+*           type: number
+*           description: Number of pen views
+*         user:
+*           type: object
+*           description: Pen author data
+*       example:
+*         _id: 63e3dbdeb1b7b5b529afde46
+*         title: Simple Card
+*         html: <div class="left"></div>
+*         css: ".left { height: 300px; width: 150px; background-color: #ce6746; }"
+*         js: "console.log(true)"
+*         likesCount: 1
+*         viewsCount: 5
+*         user: 
+*           name: Bill Gates
+*           username: billy55
+*           avatar: https://rs-clone-api.onrender.com/images/63e49eebee7d38e87d48086d.webp
+*     Pen:
+*       type: object
+*       properties:
+*         _id:
+*           type: string
+*           description: Uniq id of pen
 *         title:
 *           type: string
 *           description: Title of pen
@@ -32,19 +74,16 @@ const router = express.Router();
 *           description: Number of pen views
 *         user:
 *           type: string
-*           description: User id
-*         _id:
-*           type: string
-*           description: Pen id
+*           description: Pen author id
 *       example:
+*         _id: 63e3dbdeb1b7b5b529afde46
 *         title: Simple Card
 *         html: <div class="left"></div>
 *         css: ".left { height: 300px; width: 150px; background-color: #ce6746; }"
 *         js: "console.log(true)"
 *         likesCount: 1
 *         viewsCount: 5
-*         user: 63e3d4e4d2ba97d691114311
-*         _id: 63e3dbdeb1b7b5b529afde46
+*         user: 63e4d9af9e4d3c6019306b3f
 */
 /**
 * @swagger
@@ -52,6 +91,50 @@ const router = express.Router();
 *   name: Pens
 *   description: The pens publishing managing API
 */
+/**
+* @swagger
+* /pens:
+*   get:
+*     summary: Returns json pens data
+*     tags: [Pens]
+*     parameters:
+*       - name: _limit
+*         in: query
+*         description: Count of getting pens (default value is 10)
+*         required: false
+*         schema:
+*           type: integer
+*         style: form
+*         example: 4
+*     responses:
+*       200:
+*         description: Pen was successfully created
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*                 $ref: '#/components/schemas/PenData'
+*         headers:
+*           X-Total-Count: 
+*             description: The number of pens in database
+*             schema:
+*               type: integer
+*               example: 15237
+*       500:
+*         description: Some server error
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 message:
+*                   type: string
+*                   example: Some server error
+*/
+router.get('/', pensController.getAll);
+
 /**
 * @swagger
 * /pens:
@@ -83,7 +166,7 @@ const router = express.Router();
 *         content:
 *           application/json:
 *             schema:
-*             $ref: '#/components/schemas/PenData'
+*             $ref: '#/components/schemas/Pen'
 *       400:
 *         description: Bad request
 *         content:
