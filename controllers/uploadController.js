c31c302cb274d0fe004d29cc56c0f2f8543aca1b
Julia Kupreichik <julilike@yandex.ru>
2023-02-15 13:47:11 +0300

    fix: fix error handler

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -25,40 +25,46 @@ export const upload = multer({ storage });
 export const imgUpload = async (req, res) => {
   const defaultUrl = `${BASE_URL}/images/user-default-avatar.webp`;
   const url = `${BASE_URL}/images/${req.file.originalname}`;
-  const { avatar } = await UserModel.findOne({ _id: req.userId });
+  try {
+    const { avatar } = await UserModel.findOne({ _id: req.userId });
 
-  await UserModel.findOneAndUpdate(
-    {
-      _id: req.userId,
-    },
-    {
-      avatar: url,
-    },
-    (err) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({
-          message: 'Some server error',
-        });
+    UserModel.findOneAndUpdate(
+      {
+        _id: req.userId,
+      },
+      {
+        avatar: url,
+      },
+      (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'Some server error',
+          });
+        }
       }
+    );
+
+    if (avatar !== defaultUrl) {
+      const file = avatar.replace(`${BASE_URL}/`, '');
+
+      fs.unlink(file, (err) => {
+        if (err) {
+          return res.status(500).json({
+            message: 'Some server error',
+          });
+        } else {
+          return res.json({ avatar: url });
+        }
+      });
     }
-  );
 
-  if (avatar !== defaultUrl) {
-    const file = avatar.replace(`${BASE_URL}/`, '');
-
-    fs.unlink(file, (err) => {
-      if (err) {
-        return res.status(500).json({
-          message: 'Some server error',
-        });
-      } else {
-        return res.json({ avatar: url });
-      }
+    res.json({ avatar: url });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Some server error',
     });
   }
-
-  res.json({ avatar: url });
 };
 
 export const imgRemove = async (req, res) => {
