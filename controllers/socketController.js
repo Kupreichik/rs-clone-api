65c792de5da81eb08b2962bcec0547ffb895df82
Julia Kupreichik <julilike@yandex.ru>
2023-02-24 16:12:31 +0300

    feat: add current co-editing code saving and cursor position

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,5 +1,4 @@
 import { nanoid } from 'nanoid';
-import UserModel from '../models/User.js';
 import { io } from '../index.js';
 
 const roomIdLength = 12;
@@ -17,14 +16,14 @@ export const createRoom = async (req, res) => {
 }
 
 export const onSocketConnection = (socket) => {
-  socket.on('CODE_CHANGED', async ({ roomId, senderId, code }) => {
+  socket.on('CODE_CHANGED', async ({ roomId, code, ...data }) => {
     roomsCurrentData[roomId] = code;
-    socket.to(roomId).emit('CODE_CHANGED', { senderId, code });
+    socket.to(roomId).emit('CODE_CHANGED', { code, ...data });
   });
 
   socket.on('CONNECTED_TO_ROOM', async ({ roomId, roomUserId }) => {
     socket.join(roomId);
-    const code = roomsCurrentData[roomId];
+    const code = roomsCurrentData[roomId] || { html: '', css: '', js: '' };
     io.in(roomId).emit('START_CODE', { senderId: roomUserId, code });
   });
 };
